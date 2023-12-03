a2c7372115a782722705ffc9f9b53e1a0c4d439a
kennjoson <kennjoson@thelewiscollege.edu.ph>
2023-12-03 21:28:18 +0800

    bago

--- src/StockManagement.js
+++ src/StockManagement.js
@@ -19,7 +19,6 @@ const StockManagement = () => {
     setProdList(updatedList);
     setShowProductModal(false);
   };
-<<<<<<< HEAD
 
   const handleDeleteProduct = (index) => {
     const updatedProdList = [...prodList];
@@ -34,67 +33,6 @@ const StockManagement = () => {
       <div className="row">
         <div className="col">
           <h2 className="float-start">Stock Management</h2>
-=======
-  
-  return (
-    <div>
-      <h3>Stock Management</h3>
-      <table className="table table-responsive">
-        <thead className="text-center">
-          <tr>
-            <th scope="col" className="bg-primary text-white">Product ID</th>
-            <th scope="col" className="bg-primary text-white">Name</th>
-            <th scope="col" className="bg-primary text-white">Stock</th>
-            <th className="bg-primary text-white">Action</th>
-          </tr>
-        </thead>
-        <tbody id="tbodyproducts" className="text-center">
-          {prodList.map((product, index) => (
-            <tr key={index}>
-              <td>{product.productId}</td>
-              <td>{product.prodName}</td>
-
-              <td>{product.stock}</td>
-              <td>
-                  <button class="btn btn-primary" onClick={() => handleEditProduct(product)}>Update</button>
-                </td>   
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {showProductModal && selectedProduct && (
-    <div className="modal" style={{ display: 'block' }}>
-      <div className="modal-dialog" style={{ margin: '10% auto' }}>
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Update Stock</h5>
-            <button type="button" className="btn-close" color="none" aria-label="Close" onClick={() => setShowProductModal(false)}>
-              <span>&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <form>
-            <div className="form-group">
-              <label htmlFor="editProdStock">Stock</label>
-              <input
-                type="number" className="form-control" id="editProdStock"
-                value={selectedProduct.stock}
-                onChange={(e) => setSelectedProduct({ ...selectedProduct, stock: e.target.value })}
-              />
-            </div>
-          </form>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" onClick={() => setShowProductModal(false)}>
-            Close
-          </button>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={() => handleSaveProductEdit(selectedProduct)}>
-            Save
-          </button>
->>>>>>> d453bdb38f869610d67194164ce12f57b3b141ba
         </div>
       </div>
       <div className="row">
